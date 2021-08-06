56122840804303f3784748ff6e83f4f977ed1167
coder9218 <80843737+coder9218@users.noreply.github.com>
2021-08-06 20:24:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function setup ()
+{
+    video = createCapture(VIDEO);
+    video.size(500, 500);
+
+    canvas = createCanvas(500,500);
+    canvas.position (560,150);
+
+    poseNet = ml5.poseNet (video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw()
+{
+    background('#FFA500');
+}
+
+function modelLoaded()
+{
+    console.log (' PoseNet is Initialized!');
+}
+
+function gotPoses(results)
+{
+    if (results.length > 0)
+    {
+        console.log (results);
+    }
+}
